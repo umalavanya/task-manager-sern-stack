4616513463bce6f9192beb6c239b4d3c4beef7d5
umalavanya <umalavanya7@gmail.com>
2026-06-06 00:38:00 -0400

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -15,12 +15,13 @@ const protect = (req, res, next) => {
 
         next();
     } catch (error) {
-        
+
         return res.status(401).json({ message: 'Not authorized, token failed' });
     }
 };
 
 const authorize = (...roles) => {
+    
     return (req, res, next) => {
         if (!roles.includes(req.user.role)) {
             return res.status(403).json({ message: 'Access denied' });
