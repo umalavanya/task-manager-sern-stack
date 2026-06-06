db9210e0a7dcef1c2b1702a7a9df8ee276e501f6
umalavanya <umalavanya7@gmail.com>
2026-06-06 00:37:32 -0400

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,10 +1,11 @@
 const jwt = require('jsonwebtoken');
 
 const protect = (req, res, next) => {
-    
+
     const token = req.headers.authorization?.split(' ')[1];
     
     if (!token) {
+        
         return res.status(401).json({ message: 'Not authorized, no token' });
     }
     
