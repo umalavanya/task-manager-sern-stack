e1d13a213beb30ce89c324a9fc2190d28e1eccdc
umalavanya <umalavanya7@gmail.com>
2026-06-06 00:37:51 -0400

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -12,9 +12,10 @@ const protect = (req, res, next) => {
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
-        
+
         next();
     } catch (error) {
+        
         return res.status(401).json({ message: 'Not authorized, token failed' });
     }
 };
