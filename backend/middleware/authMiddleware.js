88bd223abb298f80223e78967892fc84ecabcbc3
umalavanya <umalavanya7@gmail.com>
2026-06-06 00:39:06 -0400

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -21,9 +21,10 @@ const protect = (req, res, next) => {
 };
 
 const authorize = (...roles) => {
-    
+
     return (req, res, next) => {
         if (!roles.includes(req.user.role)) {
+            
             return res.status(403).json({ message: 'Access denied' });
         }
         next();
